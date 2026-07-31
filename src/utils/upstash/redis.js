223b4fdebe472058258ebab9e00e1d7a265e173b
Neelesh-cts <neelesh.cts@gmail.com>
2026-07-31 10:12:29 +0530

    upstash redis fiel path update

--- src/utils/upstash/redis.js
+++
@@ -1,5 +0,0 @@
-import { Redis } from '@upstash/redis'
-export const redis = new Redis({
-  url: `${import.meta.env.VITE_UPSTASH_REDIS_REST_URL}`,
-  token: `${import.meta.env.VITE_UPSTASH_REDIS_REST_TOKEN}`,
-})
